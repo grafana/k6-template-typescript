3913cab0dc7ea003e3b207ec53be2a5e6702b699
Alex Klaus <alex.a.klaus@gmail.com>
2021-10-27 20:51:47 +1000

    Updates to WebPack cfg:   1) auto-generate entry files;   2) produce map files for compiled code;   3) copy binary files stored in `assets` folder;   4) turned off minimisation of js files;   5) keep comments in generated js files;   6) cleaned up 'package.json' file

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const GlobEntries = require('webpack-glob-entries');
 
 module.exports = {
   mode: 'production',
-  context: path.join(__dirname, 'src'),
-  entry: {
-    test1: './test1.ts',
-    test2: './test2.ts',
-  },
+  entry: GlobEntries('./src/*test*.ts'), // Generates multiple entry for each test
   output: {
     path: path.join(__dirname, 'dist'),
     libraryTarget: 'commonjs',
@@ -21,15 +19,26 @@ module.exports = {
       {
         test: /\.ts$/,
         use: 'babel-loader',
+        exclude: /node_modules/,
       },
     ],
   },
   target: 'web',
   externals: /^(k6|https?\:\/\/)(\/.*)?/,
+  // Generate map files for compiled scripts
+  devtool: "source-map",
   stats: {
     colors: true,
   },
   plugins: [
     new CleanWebpackPlugin(),
+    // Copy assets to the destination folder
+    new CopyPlugin({
+      patterns: [{ from: path.resolve(__dirname, 'assets') }],
+    }),
   ],
-}
+  optimization: {
+    // Don't minimize, as it's not used in the browser
+    minimize: false,
+  },
+};
